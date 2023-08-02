de7534046addf8fda5c49424d11a2ed6d02ec5b5
Benjamin Machock <bmachock@outlook.com>
2023-08-02 13:29:48 -0600

    fixed href link

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
         style={{ color: "white", height: 100 }}
       >
         <p class="float-end">
-          <a href="#">Back to top</a>
+          <a href="/home">Back to top</a>
         </p>
         <p>
           &copy; 2023 Kyle Jennings &middot;{" "}
