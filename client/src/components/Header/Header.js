172c229a25218968bd9d7480059a907a1a441e44
Benjamin Machock <bmachock@outlook.com>
2023-08-02 13:22:12 -0600

    fixed import

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,7 @@
-//Imports
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+// //Imports
+// import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
   //Hooks
