2d33b5bb9db8307f8977df8aef186f3a2fdb13aa
Benjamin Machock <bmachock@outlook.com>
2023-08-06 20:19:11 -0600

    styled footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,36 +1,38 @@
-//Imports
-// import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
 import { FaInstagram, FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
-// import Image from "../../images/kj_symbol.png";
 
 const Footer = () => {
-  //Hooks
-
-  //Functions
-
   return (
     <>
       <footer
-        className="container-fluid bg-dark"
-        style={{ color: "white", height: 100 }}
+        className="container-fluid bg-dark text-center"
+        style={{ color: "white", height: 150 }}
       >
-        <p class="float-end">
-          <a href="/home">Back to top</a>
-        </p>
         <p>
           &copy; 2023 Kyle Jennings
-          <a href="https://instagram.com/kylejennings13" target="no_blank">
+          <br />
+          <a
+            style={{ margin: "10px", color: "white" }}
+            href="https://instagram.com/kylejennings13"
+            target="no_blank"
+          >
             <FaInstagram />
           </a>
-          <a href="https://twitter.com/kylejennings13" target="no_blank">
+          <a
+            style={{ margin: "10px", color: "white" }}
+            href="https://twitter.com/kylejennings13"
+            target="no_blank"
+          >
             <FaTwitter />
           </a>
-          <a href="https://facebook.com/kylejenningsmusic" target="no_blank">
+          <a
+            style={{ margin: "10px", color: "white" }}
+            href="https://facebook.com/kylejenningsmusic"
+            target="no_blank"
+          >
             <FaFacebook />
           </a>
           <a
+            style={{ margin: "10px", color: "white" }}
             href="https://youtube.com/kylejenningsmusiconline"
             target="no_blank"
           >
