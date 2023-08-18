bd691031cfa644f4df8b08a2fbcfd94d3eb0a707
Benjamin Machock <bmachock@outlook.com>
2023-08-17 21:50:39 -0600

    fixed responsive navbar for desktop and mobile

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,52 +4,33 @@ import Navbar from "react-bootstrap/Navbar";
 import { LinkContainer } from "react-router-bootstrap";
 import NavLink from "react-bootstrap/esm/NavLink";
 import Logo from "../../images/kylejennings logo.png";
-import Image from "react-bootstrap/esm/Image";
+import Image from "react-bootstrap/Image";
 
 const Header = () => {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary" style={{ padding: 0 }}>
-        <Container
-          fluid
-          className="headerContainer"
-          style={{ backgroundColor: "#000" }}
-        >
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto navLinks justify-content-center flex-sm-row">
-              <LinkContainer
-                to="/schedule"
-                className="customLink"
-                style={{ color: "#e3e3e3" }}
-              >
-                <NavLink>Schedule</NavLink>
-              </LinkContainer>
-              <LinkContainer
-                to="/book"
-                className="customLink"
-                style={{ color: "#e3e3e3" }}
-              >
+      <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: "#000" }}>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <Image
+                src={Logo}
+                className="d-inline-block align-top"
+                alt="Kyle Jennings"
+              />
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle
+            style={{ background: "white" }}
+            aria-controls="responsive-navbar-nav"
+          />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto ">
+              <LinkContainer style={{ color: "white" }} to="/book">
                 <NavLink>Book</NavLink>
               </LinkContainer>
-              <LinkContainer to="/" className="logo">
-                <Navbar.Brand>
-                  <Image src={Logo} />
-                </Navbar.Brand>
-              </LinkContainer>
-              <LinkContainer
-                to="/merchandise"
-                className="customLink"
-                style={{ color: "#e3e3e3" }}
-              >
-                <NavLink>Merch</NavLink>
-              </LinkContainer>
-              <LinkContainer
-                to="/contact"
-                className="customLink"
-                style={{ color: "#e3e3e3" }}
-              >
-                <NavLink>Contact</NavLink>
+              <LinkContainer style={{ color: "white" }} to="/contact">
+                <NavLink>Contact </NavLink>
               </LinkContainer>
             </Nav>
           </Navbar.Collapse>
