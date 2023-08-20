9501941dabd838bba18d53e8f288b35f52b5aaaf
Benjamin Machock <bmachock@outlook.com>
2023-08-20 07:55:06 -0600

    completed carousel component

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -1,88 +1,43 @@
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
 import Carousel from "react-bootstrap/Carousel";
-import Kyle1 from "../../images/kyle1alt.jpg";
-import Kyle2 from "../../images/kyel2_res.jpg";
-// import Kyle2 from "../images/kyle2_res.webp";
-import Kyle3 from "../../images/kyle3.jpg";
-import Kyle4 from "../../images/kyle4.jpg";
-import Kyle5 from "../../images/kyle5.jpg";
-import Kyle6 from "../../images/kyle6.jpg";
-import Kyle7 from "../../images/kyle7.JPG";
-// import Kyle3 from "../images/kyle3.jpg";
-// import Kyle5 from "../images/kyle5.webp";
-// import ModalForm from "../components/ModalForm/ModalForm";
-// import Kyle4b from "../images/kyle4.jpg";
-// import Kyle7 from "../images/kyle7.JPG";
-// import Kyle3a from "../images/kyle3a.webp";
-// import Kyle10 from "../images/IMG_0593.JPG";
-// import Kyle11 from "../images/IMG_0596.JPG";
-// import Kyle12 from "../images/IMG_7170.JPG";
-// import Concert from "../images/KJ_Kalamazoo Social Media Post.png";
+import image1 from "../../images/carousel1.jpg";
+import image2 from "../../images/carousel2.JPG";
+import image3 from "../../images/carousel3.jpg";
+import image4 from "../../images/carousel4.jpg";
+import image5 from "../../images/carousel5.png";
 
 function KyleCarousel() {
   return (
     <>
-      <h1>Hello</h1>
-      <Carousel data-bs-theme="dark">
-        <Carousel.Item>
-          <img className="d-block w-100" src={Kyle1} alt="First slide" />
-          <Carousel.Caption>
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Kyle2} alt="Second slide" />
-          <Carousel.Caption>
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Kyle3} alt="Third slide" />
-          <Carousel.Caption>
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Kyle4} alt="Third slide" />
-          <Carousel.Caption>
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Kyle5} alt="Third slide" />
-          <Carousel.Caption>
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Kyle6} alt="Third slide" />
-          <Carousel.Caption>
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Kyle7} alt="Third slide" />
-          <Carousel.Caption>
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <Container fluid>
+        <Row>
+          <Col>
+            <Carousel data-bs-theme="dark" fade>
+              <Carousel.Item>
+                <img className="d-block w-100" src={image1} alt="First slide" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={image2}
+                  alt="Second slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img className="d-block w-100" src={image3} alt="Third slide" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img className="d-block w-100" src={image4} alt="Third slide" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img className="d-block w-100" src={image5} alt="Third slide" />
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
 // import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
+import KyleCarousel from "../components/Carousel/Carousel";
 import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+
 import Image from "react-bootstrap/Image";
 import Kyle1 from "../images/kyle1alt.jpg";
-import Kyle2 from "../images/kyle2_res.webp";
+
 import Kyle3 from "../images/kyle3.jpg";
 import Kyle5 from "../images/kyle5.webp";
 import ModalForm from "../components/ModalForm/ModalForm";
@@ -15,7 +15,6 @@ import Kyle3a from "../images/kyle3a.webp";
 import Kyle10 from "../images/IMG_0593.JPG";
 import Kyle11 from "../images/IMG_0596.JPG";
 import Kyle12 from "../images/IMG_7170.JPG";
-import Concert from "../images/KJ_Kalamazoo Social Media Post.png";
 
 const imageGroups = [
   [Kyle3a, ""],
@@ -57,45 +56,8 @@ const Home = () => {
 
   return (
     <>
+      <KyleCarousel />
       <main>
-        <Container>
-          <Row>
-            <Col xs={12} lg={6}>
-              <Image
-                src={Kyle2}
-                fluid
-                className="img"
-                style={{
-                  marginTop: "1rem",
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                  border: "5px solid white",
-                }}
-              />
-            </Col>
-            <Col xs={12} lg={6}>
-              <a
-                href="https://www.ticketmaster.com/event/08005EF8E7E0386E?fbclid=IwAR0D3NnRy73sVkRbNLYPBMB1UO9zVGwcYzAa8dGUTfk9vYqiJujrs1uuEbY"
-                target="no_blank"
-              >
-                <Image
-                  src={Concert}
-                  fluid
-                  className="img"
-                  style={{
-                    marginTop: "1rem",
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
-                    border: "5px solid white",
-                  }}
-                />
-              </a>
-            </Col>
-          </Row>
-        </Container>
-
         <div className="position-relative overflow-hidden p-1 p-md-3 m-md-1 text-center">
           <div className="col-md-6 p-lg-5 mx-auto">
             <a
