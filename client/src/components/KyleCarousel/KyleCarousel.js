724390c175ea42cc3cf77d9f09967ce05b65a894
Benjamin Machock <bmachock@outlook.com>
2023-08-27 11:41:00 -0600

    converted all carousel images to webp format

--- client/src/components/KyleCarousel/KyleCarousel.js
+++ client/src/components/KyleCarousel/KyleCarousel.js
@@ -2,11 +2,11 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import Carousel from "react-bootstrap/Carousel";
-import image1 from "../../images/carousel1.jpg";
-import image2 from "../../images/carousel2.JPG";
-import image3 from "../../images/carousel3.jpg";
-import image4 from "../../images/carousel4.jpg";
-import image5 from "../../images/carousel5.png";
+import image1 from "../../images/carousel1.webp";
+import image2 from "../../images/carousel2.webp";
+import image3 from "../../images/carousel3.webp";
+import image4 from "../../images/carousel4.webp";
+import image5 from "../../images/carousel5.webp";
 
 function KyleCarousel() {
   return (
