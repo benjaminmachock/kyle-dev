9207ccd9472e2248ae650c451167a65d9116e4ba
Benjamin Machock <bmachock@outlook.com>
2023-08-30 21:02:02 -0600

    changed button text

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
                 className="animatedBorder"
                 style={{ fontSize: 40, color: "#e3e3e3" }}
               >
-                BUY TICKETS TODAY
+                BUY TICKETS
               </Button>
             </a>
           </div>
