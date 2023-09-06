9367a6026501fb3aa3b9b56a8bb26e329d93e57f
Jordan Olguin <jordanolguin@iMac.local>
2023-09-05 21:41:24 -0600

    refactored header with spacing. deleted OG header component

--- client/src/components/Header/Header1.js
+++ client/src/components/Header/Header1.js
@@ -1,13 +1,38 @@
+import React, { useState, useEffect } from "react";
 import { LinkContainer } from "react-router-bootstrap";
 import NavLink from "react-bootstrap/esm/NavLink";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
 import Logo from "../../images/kylejennings logo.png";
+import { TbBaselineDensityMedium, TbX } from "react-icons/tb";
 
 function Header1() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const menuBtn = document.querySelector(".navbar-toggler-icon");
+
+    if (menuBtn) {
+      if (isMenuOpen) {
+        menuBtn.classList.add("cross-icon");
+      } else {
+        menuBtn.classList.remove("cross-icon");
+      }
+    }
+  }, [isMenuOpen]);
+
   return (
-    <Navbar style={{ backgroundColor: "#000" }}>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      style={{ backgroundColor: "#000" }}
+      className="headerContainer"
+    >
       <Container>
         <LinkContainer to="/">
           <Navbar.Brand>
@@ -18,7 +43,25 @@ function Header1() {
             />
           </Navbar.Brand>
         </LinkContainer>
-        <Navbar.Toggle />
+        <Navbar.Toggle
+          style={{ backgroundColor: "transparent", border: "none" }}
+          aria-controls="responsive-navbar-nav"
+          onClick={toggleMenu}
+        >
+          {isMenuOpen ? (
+            <TbX color="white" size={50} />
+          ) : (
+            <TbBaselineDensityMedium color="white" size={50} />
+          )}
+        </Navbar.Toggle>{" "}
+        <Navbar.Collapse className="justify-content-end">
+          <LinkContainer
+            style={{ color: "white", paddingLeft: "1rem" }}
+            to="/book"
+          >
+            <NavLink>Book</NavLink>
+          </LinkContainer>
+        </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end">
           <LinkContainer
             style={{ color: "white", paddingLeft: "1rem" }}
