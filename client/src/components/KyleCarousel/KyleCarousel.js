a8aa95dfc855ea7846e2ac308d492d9b36470f5f
Benjamin Machock <bmachock@outlook.com>
2023-12-16 11:46:05 -0700

    removed 4th image

--- client/src/components/KyleCarousel/KyleCarousel.js
+++ client/src/components/KyleCarousel/KyleCarousel.js
@@ -5,7 +5,6 @@ import Carousel from "react-bootstrap/Carousel";
 import image1 from "../../images/carousel1_resized_compressed.webp";
 import image2 from "../../images/carousel2_resized_compressed.webp";
 import image3 from "../../images/carousel3_compressed_resized.webp";
-import image4 from "../../images/carousel4 (2) (1).webp";
 
 function KyleCarousel() {
   return (
@@ -17,15 +16,11 @@ function KyleCarousel() {
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100" src={image1} alt="First slide" />
               </Carousel.Item>
-
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100" src={image2} alt="Third slide" />
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100" src={image3} alt="Third slide" />
-              </Carousel.Item>
-              <Carousel.Item interval={3000}>
-                <img className="d-block w-100" src={image4} alt="Third slide" />
               </Carousel.Item>
             </Carousel>
           </Col>
