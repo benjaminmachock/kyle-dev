5cb04f3c492d1c427bffa25c4a725f1cbd7305e5
Benjamin Machock <bmachock@outlook.com>
2023-12-17 10:04:12 -0700

    replacing images in carousel

--- client/src/components/KyleCarousel/KyleCarousel.js
+++ client/src/components/KyleCarousel/KyleCarousel.js
@@ -2,9 +2,11 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import Carousel from "react-bootstrap/Carousel";
-import image1 from "../../images/carousel1_resized_compressed.webp";
 import image2 from "../../images/carousel2_resized_compressed.webp";
 import image3 from "../../images/carousel3_compressed_resized.webp";
+import image4 from "../../images/kyle_concert_2.jpg";
+import image5 from "../../images/kyle1_new.jpg";
+import image6 from "../../images/kyle3_concert.JPG";
 
 function KyleCarousel() {
   return (
@@ -14,13 +16,19 @@ function KyleCarousel() {
           <Col>
             <Carousel fade id="custom-carousel">
               <Carousel.Item interval={3000}>
-                <img className="d-block w-100" src={image1} alt="First slide" />
+                <img className="d-block w-100" src={image5} alt="First slide" />
+              </Carousel.Item>
+              <Carousel.Item interval={3000}>
+                <img className="d-block w-100" src={image4} alt="Third slide" />
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100" src={image2} alt="Third slide" />
               </Carousel.Item>
               <Carousel.Item interval={3000}>
                 <img className="d-block w-100" src={image3} alt="Third slide" />
+              </Carousel.Item>
+              <Carousel.Item interval={3000}>
+                <img className="d-block w-100" src={image6} alt="Third slide" />
               </Carousel.Item>
             </Carousel>
           </Col>
